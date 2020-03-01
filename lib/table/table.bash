c48d6842781b0304f8e88d2bf86d1908c81470d1
Gcaufy <gcaufy@gmail.com>
2020-03-01 15:53:02 +0800

    fix: fixed compatible sed

--- lib/table/table.bash
+++ lib/table/table.bash
@@ -8,15 +8,8 @@ COL_SEP="[}"
 
 OS="$(uname)"
 
-if [[ "$OS" == "Darwin" ]]; then
-  ISED='-i ""'
-else
-  ISED='-i""'
-fi
-
 # Get md5 sum compatibly
 function get_md5 () {
-  OS="$(uname)"
   case $OS in
     'Linux')
       echo "$1" | md5sum | cut -c1-32
@@ -28,10 +21,19 @@ function get_md5 () {
   esac
 }
 
+# compatible sed in MacOS and Linux
+function sedi () {
+  if [[ "$OS" == "Darwin" ]]; then
+    sed -i '' "$1" "$2"
+  else
+    sed -i "$1" "$2"
+  fi
+}
+
 function table_refresh () {
-  cat "$dbpath" | grep "^#ID:" | cat > "$dbpath.tmp.deleted"
-  cat "$dbpath" | grep "^#" | grep -v "^#ID:" | cat > "$dbpath.tmp.comment"
-  cat "$dbpath" | grep -v "^#" | cat | column -t > "$dbpath.tmp.db"
+  grep "^#ID:" "$dbpath" | cat > "$dbpath.tmp.deleted"
+  grep "^#" "$dbpath" | grep -v "^#ID:" | cat > "$dbpath.tmp.comment"
+  grep -v "^#" "$dbpath" | cat | column -t > "$dbpath.tmp.db"
   cat "$dbpath.tmp.comment" "$dbpath.tmp.db" "$dbpath.tmp.deleted" > "$dbpath"
   rm "$dbpath.tmp.db" > /dev/null 2>&1
   rm "$dbpath.tmp.comment" > /dev/null 2>&1
@@ -44,8 +46,8 @@ function table_init () {
 
   if [[ ! -f "$dbpath" ]]; then
     touch "$dbpath"
-    if ! [[ -z $_columns ]]; then
-      echo "ID"$(printf "$COL_SEP%s" "${_columns[@]}") | column -t -s $"$COL_SEP" > $dbpath
+    if [[ -n $_columns ]]; then
+      echo "ID"$(printf "$COL_SEP%s" "${_columns[@]}") | column -t -s $"$COL_SEP" > "$dbpath"
     fi
   else
     table_refresh
@@ -63,81 +65,42 @@ function row_insert () {
   table_check_init
 
   local _row=( "$@" )
-  if [[ -z $_row ]]; then
+  if [[ -z ${_row[*]} ]]; then
     printf 'Error: %s\n' 'Table insert empty row' >&2
   fi
   local _rowstr=$(printf "$COL_SEP%s" "${_row[@]}")
   local _tid="ID:"$(get_md5 "$_rowstr$RANDOM" | cut -c1-10)
 
-  echo $(printf "%s$COL_SEP%s" "$_tid" "$_rowstr") | column -t -s $"$COL_SEP" >> $dbpath
+  echo $(printf "%s$COL_SEP%s" "$_tid" "$_rowstr") | column -t -s $"$COL_SEP" >> "$dbpath"
   table_refresh
-  echo $_tid
+  echo "$_tid"
 }
 
 function row_update () {
   local _row=( "$@" )
   local _id=${_row[0]}
 
-  if [[ -z $_row ]]; then
+  if [[ -z ${_row[*]} ]]; then
     printf 'Error: %s\n' 'Table insert empty row' >&2
   fi
   local _rowstr=$(printf "$COL_SEP%s" "${_row[@]}")
   _rowstr=$(echo $(printf "%s" "$_rowstr") | column -t -s $"$COL_SEP")
-  sed "$ISED" "s/^$_id.*/$_rowstr/" "$dbpath"
+  echo '----------------_UPDATE'
+  echo "$dbpath"x
+
+  sedi "s/^$_id.*/$_rowstr/" "$dbpath"
   table_refresh
 }
 function row_delete () {
   _id=$1
-  sed "$ISED" "s/^$_id/#$_id/" "$dbpath"
+  sedi "s/^$_id/#$_id/" "$dbpath"
   table_refresh
 }
 function row_select () {
   local _id=$1
   if [[ -z $_id ]]; then
-    cat $dbpath | grep "^ID:" | column -t
+    grep "^ID:" "$dbpath" | column -t
   else
-    cat $dbpath | grep "^$_id"
+    grep "^$_id" "$dbpath"
   fi
 }
-function test_row_insert () {
-  echo "======TEST: test row insert"
-  _row=( 'Jim'  'Green' '20' )
-  _id=$(row_insert ${_row[@]})
-  echo "Insert ID: $_id"
-}
-function test_row_delete () {
-  echo "======TEST: test row delete"
-  _row=( 'Jim'  'White' '21' )
-  _id=$(row_insert ${_row[@]})
-  row_delete $_id
-}
-function test_row_update () {
-  echo "======TEST: test row update"
-  local _row=( 'Jim'  'White' '21' )
-  local _id=$(row_insert ${_row[@]})
-  local _newrow=( 'Han' 'Meimei' '18' )
-  row_update "$_id" "${_newrow[@]}"
-}
-function test_row_select () {
-  echo "======TEST: test row select"
-  local _row=( "Jay-Simth"  "White" "92" )
-  local _id=$(row_insert "${_row[@]}")
-  local _newrow
-  row_select
-  read -r -a _newrow <<< $(row_select "$_id")
-  echo ${_newrow[@]} | column -t
-}
-function test_table_init () {
-  echo "======TEST: test table init"
-  local _columns=( "FirstName"  "LastName" "Age" )
-  rm /tmp/bash_table.db
-  table_init "/tmp/bash_table.db" "${_columns[@]}"
-}
-#
-# test_table_init
-# test_row_insert
-# test_row_update
-# test_row_delete
-# test_row_select
-# echo "======TEST: DONE"
-# cat /tmp/bash_table.db | column -t
